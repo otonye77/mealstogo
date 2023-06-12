469dfe7974fb3b23ff81b3a25a8816fbed0b386e
otonye77 <oamietubodie@gmail.com>
2023-06-12 20:45:31 +0100

    main

---
+++ src/features/restaurants/component/restaurants-info.component.js
@@ -0,0 +1,19 @@
+import { Text } from "react-native";
+
+const RestaurantInfo = ({ restaurant = {} }) => {
+  const {
+    name = "Some Restaurant",
+    icon,
+    photos = [
+      "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
+    ],
+    address = "100 some random street",
+    isOpenNow = true,
+    rating = 5,
+    isClosedTemporarily,
+  } = restaurant;
+  return (
+  <Text>{name}</Text>
+  );
+};
+export default RestaurantInfo;
--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,33 +1,34 @@
 import React from "react";
 import { SafeAreaView, StyleSheet, Text, View, StatusBar } from "react-native";
 import { Searchbar } from "react-native-paper";
+import RestaurantInfo from "../component/restaurants-info.component";
 
 const RestaurantsScreen = () => {
-    return (
-        <SafeAreaView style={styles.container}>
-        <View style={styles.search}>
-          <Searchbar />
-        </View>
-        <View style={styles.list}>
-          <Text>List</Text>
-        </View>
-      </SafeAreaView>
-    )
-}
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.search}>
+        <Searchbar />
+      </View>
+      <View style={styles.list}>
+        <RestaurantInfo />
+      </View>
+    </SafeAreaView>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      marginTop: StatusBar.currentHeight,
-    },
-    search: {
-      padding: 16,
-    },
-    list: {
-      flex: 1,
-      padding: 16,
-      backgroundColor: "blue",
-    },
-  });
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight,
+  },
+  search: {
+    padding: 16,
+  },
+  list: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: "blue",
+  },
+});
 
-export default RestaurantsScreen
+export default RestaurantsScreen;
