a0a3f66081a6fcf54569e8e9718e14d3cf3b2a47
otonye77 <oamietubodie@gmail.com>
2023-07-01 13:43:03 +0100

    master

---
+++ src/features/restaurants/component/spacer/spacer.component.js
@@ -0,0 +1,19 @@
+import { View } from "react-native";
+import styled from "styled-components/native";
+
+const TopSmall = styled.View`
+  margintop: 4px;
+`;
+
+const Medium = styled.View`
+  margintop: 8px;
+`;
+
+const Large = styled.View`
+  margintop: 16px;
+`;
+
+export const Spacer = ({ variant }) => {
+  switch (variant) {
+  }
+};
