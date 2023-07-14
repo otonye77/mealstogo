4b9df16b98525a40d8d92e8eb35976c4cfaef884
otonye77 <oamietubodie@gmail.com>
2023-07-14 10:44:08 +0100

    changes

--- src/features/restaurants/component/spacer/spacer.component.js
+++ src/features/restaurants/component/spacer/spacer.component.js
@@ -1,24 +1,45 @@
-import { View } from "react-native";
+import React from "react";
 import styled from "styled-components/native";
 
 const TopSmall = styled.View`
-  margintop: 4px;
+  margin-top: ${(props) => props.theme.space[1]};
 `;
 
 const TopMedium = styled.View`
-  margintop: 8px;
+  margin-top: ${(props) => props.theme.space[2]};
 `;
 
 const TopLarge = styled.View`
-  margintop: 16px;
+  margin-top: ${(props) => props.theme.space[3]};
+`;
+
+const LeftSmall = styled.View`
+  margin-left: ${(props) => props.theme.space[1]};
+`;
+
+const LeftMedium = styled.View`
+  margin-left: ${(props) => props.theme.space[2]};
+`;
+
+const LeftLarge = styled.View`
+  margin-left: ${(props) => props.theme.space[3]};
 `;
 
 export const Spacer = ({ variant }) => {
- if(variant === "top.medium"){
-    return <TopMedium />
- }
- if(variant === "top.large"){
-    return <TopLarge />
- }
- return <TopSmall />
+  if (variant === "top.medium") {
+    return <TopMedium />;
+  }
+  if (variant === "top.large") {
+    return <TopLarge />;
+  }
+  if (variant === "left.small") {
+    return <LeftSmall />;
+  }
+  if (variant === "left.medium") {
+    return <LeftMedium />;
+  }
+  if (variant === "left.large") {
+    return <LeftLarge />;
+  }
+  return <TopSmall />;
 };
